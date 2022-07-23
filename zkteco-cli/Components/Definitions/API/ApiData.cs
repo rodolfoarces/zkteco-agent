ebedd3502272492fd3774378cf842a8d8e2a531a
Rodolfo D. Arce S <info@rodolfoarce.com>
2022-07-23 16:13:22 -0400

    Rename namespace for better understating of usage - Added directory

---
+++ zkteco-cli/Components/Definitions/API/ApiData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace zkteco_cli.API
+{
+    internal class ApiData
+    {
+        string access_token { get; set; }
+        string refreshToken { get; set; }
+
+        public ApiData()
+        {
+
+        }
+
+        public string GetAccessToken()
+        {
+            return access_token;
+        }
+        public string GetRefreshToken()
+        {
+            return refreshToken;
+        }
+    }
+
+
+}
